ce863ff0ca7de062a009c55e094b6b6c6833ea1f
Mauricio Soriano <mauricio2769@gmail.com>
2020-07-29 17:34:22 -0500

    Hot Fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Inciar de sesión {{ config('app.name', 'Laravel') }}</div>
+                    <div class="card-header">Inciar sesión en {{ config('app.name', 'Laravel') }}</div>
                         <div class="card-body">
                         <form method="POST" action="{{ route('login') }}">
                             @csrf
