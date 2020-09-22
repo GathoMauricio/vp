9490be1e6f41037567b12818070cef50d3e212f3
Oscar Mauricio Juarez Soriano <mauricio2769@gmail.com>
2020-09-22 13:28:55 -0500

    testing

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -249,9 +249,20 @@ class ServiceController extends Controller
             return "<option value>--Seleccione una opción--</option>";
         }
     }
+    public function file_get_contents_curl($url) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //Set curl to return the data instead of printing it to the browser.
+        curl_setopt($ch, CURLOPT_URL, $url);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        return $data;
+    }
     public function printSErviceFormat(Request $request, $id)
     {
-        $avatarUrl = public_path("img\logo_vp.png");
+        $avatarUrl = public_path("img/logo_vp.png");
+        //$avatarUrl = $this->file_get_contents_curl($avatarUrl);
+        //return $avatarUrl;
         $arrContextOptions=array(
                         "ssl"=>array(
                             "verify_peer"=>false,
