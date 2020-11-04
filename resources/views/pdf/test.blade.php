92487a06ea5435393412521e451d56227a50fe4f
Oscar Mauricio Juarez Soriano <mauricio2769@gmail.com>
2020-11-03 19:36:00 -0600

    test function

--- resources/views/pdf/test.blade.php
+++ resources/views/pdf/test.blade.php
@@ -1,3 +1,25 @@
 @foreach($retiros as $retiro)
-<img src="{{ $retiro->firma }}" width="80" height="80"><br>
+imagen
+<img src="{{ base64_to_jpeg($retiro->firma,'jpg') }}" width="80" height="80"><br>
 @endforeach
+
+
+@php
+function base64_to_jpeg($base64_string, $output_file) { 
+    // open the output file for writing 
+    $ifp = fopen($output_file, 'wb'); 
+
+    // split the string on commas 
+    // $data[ 0 ] == "data:image/png;base64" 
+    // $data[ 1 ] == <actual base64 string> 
+    $data = explode(',', $base64_string); 
+
+    // we could add validation here with ensuring count($data) > 1 
+    fwrite($ifp, base64_decode($data[ 1 ])); 
+
+    // clean up the file resource 
+    fclose($ifp); 
+
+    return $output_file; 
+} 
+@endphp
